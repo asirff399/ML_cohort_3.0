334b2ae1cbd152adef4654816b9e5433957c25e4
Asir Intisar <aintisar48@gmail.com>
2025-01-12 22:38:11 +0600

    [Task] - ML Cohort 3.0

---
+++ Task/task_1/task_1.py
@@ -0,0 +1,4 @@
+# Task 1 => Store data using variable and data
+name = "Nahid Rana"
+wickets = 5
+is_baller = True
---
+++ Task/task_2/task_2.py
@@ -0,0 +1,21 @@
+# Task 2 
+
+# Print first 'R' from x string
+x="ABRACADABRA"
+print(x[2])
+
+# Print "brown fox" from y string
+y="The quick brown fox jumps over the lazy dog"
+print(y[10:20])
+
+# Concatenation of string
+A="বাংলা"
+B="আমার"
+C="সোনার"
+print(B+" "+C+" "+A)
+# print(f'{B} {C} {A}')
+
+# Format string modifier
+singer_name='AUR'
+song_name='Tu hain kahan'
+print(f"My favourite singer is {singer_name}, whose most popular song is {song_name}")
