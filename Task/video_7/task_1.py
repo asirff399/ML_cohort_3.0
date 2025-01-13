c3d5e2a792c356b53f9b29e27a772aabbca9bc71
Asir Intisar <aintisar48@gmail.com>
2025-01-13 15:55:46 +0600

    [Video-7] - Task: 1

---
+++ Task/video_7/task_1.py
@@ -0,0 +1,24 @@
+# Video 7 Task 1
+
+a = 7
+b = 4
+c = a | b
+d = a & b
+print(c,d)
+
+# Task = a,b -> bitwise Not Operation(^)
+e = a ^ b
+print(e)
+
+# Explaination
+
+# 1 ^ 0 = 1
+# 1 ^ 0 = 1
+# 1 ^ 1 = 0
+# 0 ^ 0 = 0
+
+#       (Binary)
+# a = 7 : 0111 
+# b = 4 : 0100 
+#         ----
+# e = 3 : 0011
