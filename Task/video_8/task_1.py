807e4ef9b82ef37758c5e94c1768478311588326
Asir Intisar <aintisar48@gmail.com>
2025-01-14 15:43:25 +0600

    [Video-8] - Task: 1

---
+++ Task/video_8/task_1.py
@@ -0,0 +1,17 @@
+# Video 8 Task 1
+
+# Using while loop print these
+
+# 1 2 3
+for i in range(1,4):
+    print(i, end=" ")
+print()
+
+# 4 5 6
+for i in range(4,7):
+    print(i, end=" ")
+print()
+
+# 7 8 9
+for i in range(7,10):
+    print(i, end=" ")
