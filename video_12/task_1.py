0866a3503d28c8bea5cae234e32dd7c52d766fee
Asir Intisar <aintisar48@gmail.com>
2025-01-17 15:20:49 +0600

    [Video-12] - Task: 1

---
+++ video_12/task_1.py
@@ -0,0 +1,18 @@
+#  Video 12 Task 1
+
+# NID Function
+
+def NID(id,name,father_name,mother_name,date_of_birth,blood_group,address):
+    print(f'ID: {id}')
+    print(f'Name: {name}')
+    print(f'Father Name: {father_name}')
+    print(f'Mother Name: {mother_name}')
+    print(f'Date Of Birth: {date_of_birth}')
+    print(f'Blood Group: {blood_group}')
+    print(f'Address: {address}')
+
+NID('3453535976','Md Asir Intisar','Golam Rabbani','Rabeya Sultana Golapy','16.6.2004','A+','Chapainawabganj, Rajshahi, Bangladesh')
+print()
+NID('8635867957','-_-_-_-','-_-_-_-','-_-_-_-','9.2.2003','A+','Khulna, Bangladesh')
+print()
+NID('6352487953','-_-_-_-','-_-_-_-','-_-_-_-','18.11.2005','A+','Dhaka, Bangladesh')
